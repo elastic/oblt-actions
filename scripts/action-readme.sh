5b3fc90cc58b1dc093f4fed0a0dd267382ca5886
Jan Calanog <jan.calanog@elastic.co>
2024-06-04 09:36:22 +0200

    fixes

--- scripts/action-readme.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-find . \( -name "action.yml" -o -name "action.yaml" \) -exec dirname {} \; | while read -r dir; do
-    printf "%s: " "$dir"
-    gh action-readme update --action="$dir/action.yml" --readme="$dir/README.md"
-done
