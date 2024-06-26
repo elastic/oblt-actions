435f122652635744a9e433d9f3fffec64a6f2488
Jan Calanog <jan.calanog@elastic.co>
2024-06-27 01:19:07 +0200

    ok

--- oblt-cli/cluster-create-ccs/summary.js
+++ oblt-cli/cluster-create-ccs/summary.js
@@ -4,7 +4,7 @@ module.exports = async ({github, context, core}) => {
     const clusterInfo = JSON.parse(fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/cluster-info.json`, 'utf8'))
     const statusUrl = `https://github.com/elastic/observability-test-environments/pulls?q=is%3Apr+${clusterInfo.ClusterName}`
     await core.summary
-        .addHeading('oblt-cli')
+        .addHeading('oblt-cli', '3')
         .addTable([
             [{data: 'Cluster Name', header: true}, {data: 'Stack Version', header: true}, {data: 'Status', header: true}],
             [clusterInfo.ClusterName, clusterInfo.StackVersion, clusterInfo.RemoteClusterName],
