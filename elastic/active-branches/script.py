d457b2fb9e714344b879ee10ea3b9d993b3c6ffc
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-12-11 16:25:01 +0100

    feat: support branches only active in the current repository

--- elastic/active-branches/script.py
+++ elastic/active-branches/script.py
@@ -26,6 +26,29 @@ exclude_branches = set(filter(lambda branch: len(branch) > 0, exclude_branches.s
 if exclude_branches:
     branches = list(filter(lambda branch: branch not in exclude_branches, branches))
 
+# Filter branches based on the existence in the GitHub repository
+filter_str = os.environ.get('FILTER', 'false')
+filter = filter_str.lower() in ('true', '1', 't', 'y', 'yes')
+repository = os.environ.get('REPOSITORY', '')
+github_token = os.environ.get('GITHUB_TOKEN', '')
+if filter and repository and github_token:
+    # Check if branches exist in the GitHub repository
+    headers = {
+        'Authorization': f'token {github_token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+
+    existing_branches = []
+    for branch in branches:
+        branch_url = f'https://api.github.com/repos/{repository}/branches/{branch}'
+        response = requests.get(branch_url, headers=headers)
+        if response.status_code == 200:
+            existing_branches.append(branch)
+        else:
+            print(f"Branch {branch} does not exist in the repository")
+
+    branches = existing_branches
+
 include_branches = list(map(lambda branch: {"branch": branch}, branches))
 matrix = {'include': include_branches}
 
