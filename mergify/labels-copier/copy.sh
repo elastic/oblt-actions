8003b5fde8093205e80d5bcae72ed50327987b9b
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-01-08 17:30:32 +0100

    fix

--- mergify/labels-copier/copy.sh
+++ mergify/labels-copier/copy.sh
@@ -18,7 +18,7 @@ if [ -n "$RUNNER_DEBUG" ] ; then
   set -x
 fi
 
-pr_number=$(gh pr view --json body -q ".body" $PR_URL | sed -n -e '/automatic backport of pull request/,/done/p' | cut -d"#" -f2 | cut -d" " -f1)
+pr_number=$(gh pr view --json body -q ".body" "$PR_URL" | sed -n -e '/automatic backport of pull request/,/done/p' | cut -d"#" -f2 | cut -d" " -f1)
 gh pr view --json labels -q '.labels[]|.name' ${REPOSITORY_URL}/pull/$pr_number | while read label ; do
   if [[ -z "$labels" ]] || [[ ",$labels," =~ ",$label," ]]; then
     # Check if the label is excluded by regex
@@ -26,19 +26,19 @@ gh pr view --json labels -q '.labels[]|.name' ${REPOSITORY_URL}/pull/$pr_number 
     if [[ $label =~ $EXCLUDED_LABEL ]]; then
       excluded=true
     fi
-    if [[ "$excluded" == "false" ]] ; then
-      if [[ "$DRY_RUN" == "true" ]] ; then
+    if [ "$excluded" == "false" ] ; then
+      if [ "$DRY_RUN" == "true" ] ; then
         echo ">>> $label"
       else
-        gh pr edit --add-label "$label" $PR_URL
+        gh pr edit --add-label "$label" "$PR_URL"
       fi
     fi
   fi
   for additional_label in $(echo $ADDITIONAL_LABELS | sed "s/,/ /g") ; do
-    if [[ "$DRY_RUN" == "true" ]] ; then
+    if [ "$DRY_RUN" == "true" ] ; then
       echo ">>> $additional_label"
     else
-      gh pr edit --add-label "$additional_label" $PR_URL
+      gh pr edit --add-label "$additional_label" "$PR_URL"
     fi
   done
 done
