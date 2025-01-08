bb0813130aac7738c9e30b2dce55ad70655e82e5
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-01-08 17:24:18 +0100

    fix and add verbose

--- mergify/labels-copier/copy.sh
+++ mergify/labels-copier/copy.sh
@@ -13,6 +13,10 @@
 
 set -eo pipefail
 
+if [ -n "$RUNNER_DEBUG" ] ; then
+  set -x
+fi
+
 pr_number=$(gh pr view --json body -q ".body" $PR_URL | sed -n -e '/automatic backport of pull request/,/done/p' | cut -d"#" -f2 | cut -d" " -f1)
 gh pr view --json labels -q '.labels[]|.name' ${REPOSITORY_URL}/pull/$pr_number | while read label ; do
   if [[ -z "$labels" ]] || [[ ",$labels," =~ ",$label," ]]; then
@@ -29,7 +33,7 @@ gh pr view --json labels -q '.labels[]|.name' ${REPOSITORY_URL}/pull/$pr_number 
       fi
     fi
   fi
-  for additional_label in $(echo $v | sed "s/,/ /g") ; do
+  for additional_label in $(echo $ADDITIONAL_LABELS | sed "s/,/ /g") ; do
     if [[ "$DRY_RUN" == "true" ]] ; then
       echo ">>> $additional_label"
     else
