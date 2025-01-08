5f6f45b9122fa3b27cd1e73a6b390afd5ae6f3c1
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-01-08 17:44:29 +0100

    refactor

--- mergify/labels-copier/copy.sh
+++ mergify/labels-copier/copy.sh
@@ -19,24 +19,28 @@ if [ -n "$RUNNER_DEBUG" ] ; then
   set -x
 fi
 
+add_label() {
+  local label=$1
+  local pr_url=$2
+
+  if [ "$DRY_RUN" == "true" ]; then
+    echo ">> [dry-run]: $label will be added"
+  else
+    gh pr edit --add-label "$label" "$pr_url"
+  fi
+}
+
+
 pr_number=$(gh pr view --json body -q ".body" "$PR_URL" | sed -n -e '/automatic backport of pull request/,/done/p' | cut -d"#" -f2 | cut -d" " -f1)
 gh pr view --json labels -q '.labels[]|.name' ${REPOSITORY_URL}/pull/$pr_number | while read label ; do
   if [[ -z "$labels" ]] || [[ ",$labels," =~ ",$label," ]]; then
     if [[ -n "$EXCLUDED_LABEL" ]] && [[ $label =~ $EXCLUDED_LABEL ]]; then
       echo ">> $label is excluded and will not be added since matches '$EXCLUDED_LABEL'"
     else
-      if [ "$DRY_RUN" == "true" ] ; then
-        echo ">> [dry-run]: $label will be added"
-      else
-        gh pr edit --add-label "$label" "$PR_URL"
-      fi
+      add_label "$label" "$PR_URL"
     fi
   fi
   for additional_label in $(echo $ADDITIONAL_LABELS | sed "s/,/ /g") ; do
-    if [ "$DRY_RUN" == "true" ] ; then
-      echo ">> [dry-run]: $label will be added"
-    else
-      gh pr edit --add-label "$additional_label" "$PR_URL"
-    fi
+    add_label "$additional_label" "$PR_URL"
   done
 done
